00a1d5624b1a2b2b3b0f9b790f4a84c72a58238c
JackCurtis <jack.v.curtis@gmail.com>
2016-03-27 11:33:21 -0400

    same as last commit - remove unused code

--- public/modules/monsters/services/monster-utils.js
+++ public/modules/monsters/services/monster-utils.js
@@ -1,29 +1,3 @@
 module.exports = function MonsterUtils(StatusEnumerable){
 
-	this.addItem = function (enumId, itemId, dataArray) {	
-		var item;
-		var enumType = this.getEnumerable(enumId);
-
-		if (enumType.list) {
-			item = enumType.list().filter(function (item){
-				return item.id == itemId.toLowerCase();
-			})[0];
-		} 
-		else {
-			item = itemId;
-		}
-
-		if (item) {
-			dataArray.push(item);
-			return StatusEnumerable.OK();
-		}
-		else {
-			return StatusEnumerable.InputError(itemId + " is not a legal value");
-		}
-	};
-
-	this.deleteItem = function (index, dataArray) {
-		dataArray.splice(index, 1);
-	};
-
 };
